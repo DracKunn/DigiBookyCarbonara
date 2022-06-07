83571a537ff6e28ec15d38b5f780f2c0c8251963
Andy De Gheldere <andy.degheldere@gmail.com>
2022-06-07 11:53:46 +0200

    initial commit

---
+++ src/main/java/com/carbonaracode/digibookycarbonara/DigiBookyCarbonaraApplication.java
@@ -0,0 +1,13 @@
+package com.carbonaracode.digibookycarbonara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiBookyCarbonaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiBookyCarbonaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carbonaracode/digibookycarbonara/DigiBookyCarbonaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbonaracode.digibookycarbonara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiBookyCarbonaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
